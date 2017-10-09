de63cbc6e026349fcb06af97fe7e53b0f4436e14
GGruwez <gilles.gruwez@yahoo.com>
2017-10-09 14:55:34 +0200

    airplane

---
+++ src/Airplane.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author Gilles
+ *
+ */
+public class Airplane {
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param xVelocity
+	 * @param yVelocity
+	 * @param zVelocity
+	 * @param mass
+	 * @param thrust
+	 * @param leftWingInclination
+	 * @param rightWingInclination
+	 * @param horStabInclination
+	 * @param verStabInclination
+	 */
+	public Airplane(float x, float y, float z, float xVelocity, float yVelocity, float zVelocity,
+			float mass, float thrust, float leftWingInclination,float rightWingInclination,
+			float horStabInclination, float verStabInclination ){
+		
+	}
+}
