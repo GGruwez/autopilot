4ff31e077dc259c17f48cf761c924c164be9ecc4
crouxderoo <ruben.croux@hotmail.com>
2017-10-18 17:51:52 +0200

    nog wat geprul

--- src/InputToOutput.java
+++ src/InputToOutput.java
@@ -15,23 +15,27 @@ public class InputToOutput {
         float horStabInclination;
         //eerst draaien
         if (imageVector[0]!=0) {
-            float r = x/(nbColumns/2)*60;
+            float r = x/(nbColumns/2)*autopilot.config.getHorizontalAngleOfView();
             float U0 = -(input.getZ()-prev.getZ())/prev.getElapsedTime()-input.getElapsedTime();
             float W0 = (input.getY()-prev.getY())/prev.getElapsedTime()-input.getElapsedTime();
             float g = autopilot.config;
             float omega = input.getPitch();
             float p = (float) (r*U0/W0 - g/W0*Math.sin(omega));
-
-
+            if (x>0) {
+                //formule Simon
+            }
+            else {
+                //formule Simon
+            }
         }
         //daarna omhoog/omlaag
         else if(imageVector[1]!=0) {
-
+            float r = y/(nbRows/2)*autopilot.config.getHorizontalAngleOfView();
+            horStabInclination = r; //??
         }
 
-
         return new AutopilotOutputs() {
-            public float getThrust() { return 0; }
+            public float getThrust() { return 0; } //moet nog over nagedacht worden
             public float getLeftWingInclination() { return leftWingInclination; }
             public float getRightWingInclination() { return rightWingInclination; }
             public float getHorStabInclination() { return horStabInclination; }
