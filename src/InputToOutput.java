17b3f9e6acc410ab97f763d5d9eb6eceefbd8900
Stien Vanderhallen <Stien@MacBook-Pro-van-Hans.local>
2017-10-19 11:40:22 +0200

    /

--- src/InputToOutput.java
+++ src/InputToOutput.java
@@ -4,16 +4,19 @@ public class InputToOutput {
     public AutopilotOutputs calculate(AutopilotInputs input, float[] imageVector, int nbColumns, int nbRows, Autopilot autopilot) {
         float x = imageVector[0];
         float y = imageVector[1];
-        float leftWingInclination;
-        float rightWingInclination;
-        float horStabInclination;
+        float leftWingInclination = 0;
+        float rightWingInclination = 0;
+        float horStabInclination = 0;
+        float verStabInclination = 0;
+        float thrust = 0;
+        
         AutopilotInputs prev = autopilot.getPreviousInput();
         //eerst draaien
         if (imageVector[0]!=0) {
             float r = x/(nbColumns/2)*autopilot.config.getHorizontalAngleOfView();
             float U0 = -(input.getZ()-prev.getZ())/prev.getElapsedTime()-input.getElapsedTime();
             float W0 = (input.getY()-prev.getY())/prev.getElapsedTime()-input.getElapsedTime();
-            float g = autopilot.config;
+            float g = autopilot.config.getGravity();
             float omega = input.getPitch();
             float p = (float) (r*U0/W0 - g/W0*Math.sin(omega));
             if (x>0) {
@@ -33,12 +36,7 @@ public class InputToOutput {
 
         }
 
-        return new AutopilotOutputs() {
-            public float getThrust() { return 0; } //moet nog over nagedacht worden
-            public float getLeftWingInclination() { return leftWingInclination; }
-            public float getRightWingInclination() { return rightWingInclination; }
-            public float getHorStabInclination() { return horStabInclination; }
-            public float getVerStabInclination() { return 0; }
+        return new AutopilotOutputs(thrust, leftWingInclination, rightWingInclination, horStabInclination, verStabInclination) {
         };
     }
 
