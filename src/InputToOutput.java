d94a6cdcf70f9ef836e2fa5e6205e173fb1232f9
Simon Smeets <simon-smeets@hotmail.be>
2017-10-19 12:58:30 +0200

    changing turning autopilot

--- src/InputToOutput.java
+++ src/InputToOutput.java
@@ -1,34 +1,28 @@
 import p_en_o_cw_2017.*;
 public class InputToOutput {
 
-<<<<<<< HEAD
     public AutopilotOutputs calculate(AutopilotInputs input, float[] targetVector, int nbColumns, int nbRows, Autopilot autopilot) {
-        float x = targetVector[0];
-        float y = targetVector[1];
+        float horizontalError = targetVector[0];
+        float verticalError = targetVector[1];
         float leftWingInclination;
         float rightWingInclination;
         float horStabInclination;
-=======
-    public AutopilotOutputs calculate(AutopilotInputs input, float[] imageVector, int nbColumns, int nbRows, Autopilot autopilot) {
-        float x = imageVector[0];
-        float y = imageVector[1];
-        float leftWingInclination = 0;
-        float rightWingInclination = 0;
-        float horStabInclination = 0;
-        float verStabInclination = 0;
-        float thrust = 0;
-        
->>>>>>> 17b3f9e6acc410ab97f763d5d9eb6eceefbd8900
+        Vector velocityWorld;
+        Vector velocityDrone;
         AutopilotInputs prev = autopilot.getPreviousInput();
+        
+        velocityWorld = new Vector((input.getX()-prev.getX())/(prev.getElapsedTime()-input.getElapsedTime()),(input.getY()-prev.getY())/(prev.getElapsedTime()-input.getElapsedTime()),(input.getZ()-prev.getZ())/(prev.getElapsedTime()-input.getElapsedTime()));
+        velocityDrone = velocityWorld.inverseTransform(prev.getHeading(),prev.getPitch(),prev.getRoll());
         //eerst draaien
-        if (targetVector[0]!=0) {
-            float r = x/(nbColumns/2)*autopilot.config.getHorizontalAngleOfView();
-            float U0 = -(input.getZ()-prev.getZ())/prev.getElapsedTime()-input.getElapsedTime();
-            float W0 = (input.getY()-prev.getY())/prev.getElapsedTime()-input.getElapsedTime();
+        
+        if (horizontalError >= 10) {
+        	
+        	
+            float horizontalAngleError = horizontalError/((nbColumns/2)*autopilot.config.getHorizontalAngleOfView());
             float g = autopilot.config.getGravity();
-            float omega = input.getPitch();
-            float p = (float) (r*U0/W0 - g/W0*Math.sin(omega));
-            if (x>0) {
+            float pitch = input.getPitch();
+            float targetRoll = (float) (horizontalAngleError*-velocityDrone.getZ()/velocityDrone.getY() - g/velocityDrone.getY()*Math.sin(pitch));
+            if (horizontalError>0) {
                 //formule Simon
             }
             else {
@@ -37,7 +31,7 @@ public class InputToOutput {
         }
         //daarna omhoog/omlaag
         else if(targetVector[1]!=0) {
-            float r = y/(nbRows/2)*autopilot.config.getHorizontalAngleOfView();
+            float r = verticalError/(nbRows/2)*autopilot.config.getHorizontalAngleOfView();
             horStabInclination = r; //??
         }
         //anders gewoon blijven verder vliegen in een rechte lijn
@@ -45,7 +39,12 @@ public class InputToOutput {
 
         }
 
-        return new AutopilotOutputs(thrust, leftWingInclination, rightWingInclination, horStabInclination, verStabInclination) {
+        return new AutopilotOutputs() {
+            public float getThrust() { return 0; } //moet nog over nagedacht worden
+            public float getLeftWingInclination() { return leftWingInclination; }
+            public float getRightWingInclination() { return rightWingInclination; }
+            public float getHorStabInclination() { return horStabInclination; }
+            public float getVerStabInclination() { return 0; }
         };
     }
 
