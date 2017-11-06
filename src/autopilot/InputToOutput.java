1d48c84da5f7fb1667ea07e4b3bc6e01d250309c
Stien Vanderhallen <Stien@MacBook-Pro-van-Hans.local>
2017-11-06 17:40:09 +0100

    /

--- src/autopilot/InputToOutput.java
+++ src/autopilot/InputToOutput.java
@@ -2,9 +2,7 @@ package autopilot;
 import p_en_o_cw_2017.*;
 class InputToOutput {
 
-	static PIDcontroller PitchController = new PIDcontroller(0.5f,0,Float.MAX_VALUE);
-	
-	
+	static PIDcontroller PitchController = new PIDcontroller(0.7f, 0f, 3f);
 	
     static AutopilotOutputs calculate(AutopilotInputs input, float[] targetVector, int nbColumns, int nbRows, Autopilot autopilot) {
         PreviousInputs prev = autopilot.getPreviousInput();
@@ -23,17 +21,15 @@ class InputToOutput {
         
         velocityWorld = new Vector((input.getX()-prev.getX())/dt,(input.getY()-prev.getY())/dt,(input.getZ()-prev.getZ())/dt);
         velocityDrone = velocityWorld.inverseTransform(prev.getHeading(),prev.getPitch(),prev.getRoll());
-        Vector angularVelocity = new Vector((input.getPitch()-prev.getPitch())/dt,(input.getHeading()-prev.getHeading())/dt,(input.getRoll()-prev.getRoll())/dt);
+        Vector angularVelocity = (new Vector((input.getPitch()-prev.getPitch())/dt,(input.getHeading()-prev.getHeading())/dt,(input.getRoll()-prev.getRoll())/dt));
         
-        
-        horStabInclination = PitchController.getOutput(dt, input.getPitch(), 0);
+        horStabInclination = PitchController.getOutput(input.getPitch(), 0);
         if (input.getPitch() + horStabInclination > Math.PI/9){
         	horStabInclination = (float) (Math.PI/9);
         }
         else if(input.getPitch() + horStabInclination < -Math.PI/9){
         	horStabInclination = (float) (-Math.PI/9);
         }
-        
         
         return new AutopilotOutputs(thrust, leftWingInclination, rightWingInclination, horStabInclination, verStabInclination);
     }
--- src/autopilot/PIDcontroller.java
+++ src/autopilot/PIDcontroller.java
@@ -2,73 +2,44 @@ package autopilot;
 
 public class PIDcontroller {
 	
-	private float K ;
-	private float Ti;
-	private float Td;
-	private float processValue;
-	private float reference;
-	private float alfa = 0.1f;
-	private float previousEDf;
-	private float previousPreviousEDf;
-	private float previousError;
-	private float previousOutput;
-	private float Ts;
+	private float Kp;
+	private float Ki;
+	private float Kd;
+	private float errorSum;
+	private float errorPrevious;
 	
-	PIDcontroller(float k , float ti, float td){
-		this.setConfig(k, ti, td);
+	PIDcontroller(float Kp, float Ki, float Kd) {
+		this.setControl(Kp, Ki, Kd);
 	}
 	
-	public float getEDf() {
-		return previousEDf/(Ts/getTf()+1) + getError()*Ts/getTf()/(Ts/getTf()+1);
+	public void setControl(float Kp, float Ki, float Kd) {
+		this.Kp = Kp;
+		this.Ki = Ki;
+		this.Kd = Kd;
 	}
 	
-	public float getTf() {
-		return alfa*Td;
+	public float getErrorSum() {
+		return this.errorSum;
 	}
 	
-	
-	public float getDelta() {
-		float P = getError()-previousError;
-		float I = Ts/Ti*getError();
-		float D = Td/Ts*(getEDf()-2*previousEDf+previousPreviousEDf);
-		return K*(P+I+D);
+	public void setErrorSum(float sum) {
+		this.errorSum = sum;
 	}
 	
-	public float getError() {
-		return processValue-reference;
+	public float getPreviousError() {
+		return this.errorPrevious;
 	}
 	
-
-	private void setConfig(float K, float Ti, float Td){
-		this.K = K;
-		this.Ti= Ti;
-		this.Td = Td;
+	public void setPreviousError(float error) {
+		this.errorPrevious = error;
 	}
 	
-	public void setProcessValue(float value) {
-		this.processValue = value;
-	}
-	
-	public void setReference(float reference){
-		this.reference = reference;
+	public float getOutput(float y, float ref) {
+		float error = ref-y;
+		float delta = error-getPreviousError();
+		setErrorSum(0.5f*getErrorSum()+error);
+		setPreviousError(error);
+		return Kp*(error) + Ki*getErrorSum() + Kd*(delta);
 	}
 
-	public void setTs(float dt) {
-		this.Ts = dt;
-	}
-	
-	public float getOutput(float dt, float processValue, float reference) {
-		setTs(dt);
-		setProcessValue(processValue);
-		setReference(reference);
-		float output = getDelta()+previousOutput;
-		previousOutput = output;
-		previousError = getError();
-		previousPreviousEDf = previousEDf;
-		previousEDf = getEDf();
-		return output;
-	}
-	
-	
-	
 }
